589b2305b5c798fdfe4b3e07f5cb7a2b082210b3
Carney, Sarah A <sacarney@iastate.edu>
2017-08-03 11:33:26 -0500

    D8-129 Got mobile menus working

---
+++ js/iastate_theme.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * javascript for the ISU Theme.
+ */
+
+(function ($, Drupal) {
+ /* Toggle Menu Navbar and Site Links on Mobile */
+
+ $(document).ready(function() {
+  $('#isu-menu-navbar_toggler').click(function() {
+    $('#isu-menu-navbar_collapse').toggleClass('isu-menu-navbar_show');
+    $('#isu-sitelinks_collapse').toggleClass('isu-sitelinks_show');
+  });
+ })
+
+ /* Toggle Search on Mobile */
+
+ $(document).ready(function() {
+  $('#isu-search_toggler').click(function() {
+    $('#isu-search_collapse').toggleClass('isu-search_show');
+  })
+ })
+
+ })(jQuery, Drupal);
