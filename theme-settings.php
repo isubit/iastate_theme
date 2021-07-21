ab911013c3c23ee8fc30be386879084bc5434c8b
sacarney <sacarney@iastate.edu>
2021-07-21 15:37:40 -0500

    D8ISUTHEME-157 Add footer logo dimensions settings

--- theme-settings.php
+++ theme-settings.php
@@ -378,4 +378,27 @@ function iastate_theme_form_system_theme_settings_alter(&$form, &$form_state) {
     '#default_value'  => theme_get_setting('iastate_footer_logo_alt'),
     '#description' => t('If left blank the alt text will be Iowa State University logo'),
   );
+
+  // Footer logo dimensions description
+    $form['iastate_footer_logo']['footer_logo_dimensions_description'] = array(
+    '#type' => 'item',
+    '#title' => t('Logo dimensions'),
+    '#markup' => t('Set an explicit width and height for the footer logo.'),
+  );
+
+  // Footer logo width
+  $form['iastate_footer_logo']['iastate_footer_logo_width'] = array(
+    '#type'   => 'number',
+    '#title'  => t('Footer logo width in pixels'),
+    '#default_value'  => theme_get_setting('iastate_footer_logo_width'),
+    '#description' => t('Default is 180.'),
+  );
+
+  // Footer logo height
+  $form['iastate_footer_logo']['iastate_footer_logo_height'] = array(
+    '#type'   => 'number',
+    '#title'  => t('Footer logo height in pixels'),
+    '#default_value'  => theme_get_setting('iastate_footer_logo_height'),
+    '#description' => t('Default is 58.'),
+  );
 }
